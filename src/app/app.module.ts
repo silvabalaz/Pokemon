cb133da9bdc05033485b6a664b580a7121cd0443
silva <silva.haberl@gmail.com>
2020-12-13 19:22:57 +0100

    create welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
     BrowserModule
   ],
   providers: [],
