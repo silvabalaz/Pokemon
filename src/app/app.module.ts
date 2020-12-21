886c2f7a3b2136fe2a1f9a9161e3a0bde0939437
silva <silva.haberl@gmail.com>
2020-12-21 19:01:51 +0100

    routing version one component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { VersionOneComponent } from './version-one/version-one.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path: 'versionOne', component:  VersionOneComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VersionOneComponent
   ],
   imports: [
     BrowserAnimationsModule,
     MatButtonModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
