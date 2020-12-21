c68c520f60c34d494f879eb25efa80dfb6daa803
silva <silva.haberl@gmail.com>
2020-12-21 19:03:04 +0100

    create version one component

---
+++ src/app/version-one/version-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-version-one',
+  templateUrl: './version-one.component.html',
+  styleUrls: ['./version-one.component.css']
+})
+export class VersionOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
